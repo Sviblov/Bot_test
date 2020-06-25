4760cac06fcbf0aa13128c59f53bb0ef3687b2d5
Igor <igor.fedotov89@gmail.com>
2020-06-25 13:47:07 +0300

    Final

--- SvblvBot.py
+++ SvblvBot.py
@@ -12,7 +12,10 @@ user_state={}
 
 def sum_or_mult(list, mult_flag):
     try:
-        numbers_list = [float(el) for el in list.split()]
+        if ';' in list:
+            numbers_list = [float(el) for el in list.split(';')]
+        else:
+            numbers_list = [float(el) for el in list.split()]
         if mult_flag:
             return numpy.prod(numbers_list)
         else:
@@ -23,29 +26,37 @@ def sum_or_mult(list, mult_flag):
 @bot.message_handler(commands=['start'])
 def send_welcome(message: Message):
     global user_state
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}\n\nCначала выбери команду:\n   ➕сложение /sum\n   ✖️умножение /mult. \nЗатем отправляй боту числа через пробел, а он будет их соответственно складывать или перемножать')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}\n\nCначала выбери команду:\n   ➕сложение /sum\n   ✖️умножение /mult. \nЗатем отправляй боту числа через пробел или разделенные ; , а он будет их соответственно складывать или перемножать')
     user_state[message.from_user.id]=False
+    with open('log_svblv_bot.txt','a') as f:
+        print(f'User: {message.from_user.first_name} Message: {message.text}',file = f)
 
 @bot.message_handler(commands=['sum'])
 def change_flag(message: Message):
     global user_state
-    if user_state[message.from_user.id]:
-        user_state[message.from_user.id] = not user_state[message.from_user.id]
-    bot.send_message(message.chat.id, 'Теперь отправляйте через пробел числа, бот их сложит')
+    user_state[message.from_user.id] =False
+    bot.send_message(message.chat.id, 'Теперь отправляйте числа через пробел, или разделенные ; , бот их сложит:\nНапример:\n\n1 2 3 4 5\nили\n1;2;3;4;5')
+    with open('log_svblv_bot.txt','a') as f:
+        print(f'User: {message.from_user.first_name} Message: {message.text}',file = f)
 
 @bot.message_handler(commands=['mult'])
 def change_flag(message: Message):
     global user_state
-    if not user_state[message.from_user.id]:
-        user_state[message.from_user.id] = not user_state[message.from_user.id]
-    bot.send_message(message.chat.id, 'Теперь отправляйте через пробел числа, бот их перемножит')
+    user_state[message.from_user.id] = True
+    bot.send_message(message.chat.id, 'Теперь отправляйте числа через пробел, или разделенные ; , бот их перемножит:\nНапример:\n\n1 2 3 4 5\nили\n1;2;3;4;5')
+    with open('log_svblv_bot.txt','a') as f:
+        print(f'User: {message.from_user.first_name} Message: {message.text}',file = f)
 
 @bot.message_handler(func = lambda message: True)
 def result(message: Message):
     global mult_flag
     global user_state
-    output = sum_or_mult(message.text,user_state[message.from_user.id])
+    if user_state.get(message.from_user.id) is not None:
+        output = sum_or_mult(message.text,user_state[message.from_user.id])
+    else:
+        output = 'Я перезагрузил бота. Нажми еще раз\n/start'
     bot.send_message(message.chat.id , output)
-
+    with open('log_svblv_bot.txt','a') as f:
+        print(f'User: {message.from_user.first_name} Message: {message.text}',file = f)
 
 bot.polling()
--- Test_bot.py
+++ Test_bot.py
@@ -6,12 +6,6 @@ import pprint as pp
 
 base_url = 'https://api.telegram.org/bot1237869167:AAHlSqvq9Kw5Me9g4zrCAaaVya_yCLe9s80/'
 
-r = requests.get(f'{base_url}getMe')
-
 r = requests.get(f'{base_url}getUpdates')
 
-answer = {}
-answer['chat_id']=173409214
-answer['text'] = 'blablabla'
-
-r = requests.get(f'{base_url}sendMessage', data=answer)
+pp.pprint(r.json())
